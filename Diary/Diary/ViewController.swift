233a42475a96503d3a9fe090481b477f3a306f36
KayAhn0126 <dkseotn123@gmail.com>
2022-11-26 21:58:14 +0900

    feat: Diary 프로젝트 추가

---
+++ Diary/Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Kay on 2022/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
