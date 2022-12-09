04e7a93f720539b6f545d6d0d21cf3e9675a7767
KayAhn0126 <dkseotn123@gmail.com>
2022-12-09 23:22:14 +0900

    feat: 새로운 파일 추가

---
+++ Diary/Diary/Diary/DiaryModel/Diary.swift
@@ -0,0 +1,8 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Kay on 2022/12/07.
+//
+
+import Foundation
---
+++ Diary/Diary/Protocols/WriteDiaryDelegateProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  WriteDiaryDelegateProtocol.swift
+//  Diary
+//
+//  Created by Kay on 2022/12/07.
+//
+
+import Foundation
