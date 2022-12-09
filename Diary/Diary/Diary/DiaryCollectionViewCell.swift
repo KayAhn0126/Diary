283013299425eae7e81ce9d608c22c3fe0e576d6
KayAhn0126 <dkseotn123@gmail.com>
2022-12-09 23:23:46 +0900

    refactor: cell을 구성하는 메서드 구현

--- Diary/Diary/Diary/DiaryCollectionViewCell.swift
+++ Diary/Diary/Diary/DiaryCollectionViewCell.swift
@@ -11,4 +11,16 @@ class DiaryCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    
+    func configure(diary: Diary) {
+        let borderColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0)
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderWidth = 0.5
+        self.layer.cornerRadius = 5.0
+        titleLabel.text = diary.titile
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        dateLabel.text = formatter.string(from: diary.date)
+    }
 }
