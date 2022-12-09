321401ccac0e5fc81b82a981f7c87f9137d7c131
KayAhn0126 <dkseotn123@gmail.com>
2022-12-09 23:36:49 +0900

    refactor: collectionView 구현 및 데이터가 추가될때 snapshot으로 reload 메서드 대체 기능 구현

--- Diary/Diary/Diary/DiaryViewController.swift
+++ Diary/Diary/Diary/DiaryViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class DiaryViewController: UIViewController {
+enum Section {
+    case main
+}
 
+class DiaryViewController: UIViewController {
+    typealias Item = Diary
     @IBOutlet weak var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    private var diaryList = [Diary]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDataSource()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let wrietDiaryViewController = segue.destination as? WriteDiaryViewController {
+            wrietDiaryViewController.delegate = self
+        }
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DiaryCollectionViewCell", for: indexPath) as? DiaryCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.configure(diary: item)
+            return cell
+        })
+        collectionView.collectionViewLayout = configureLayout()
+    }
+    
+    private func configureLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        group.interItemSpacing = NSCollectionLayoutSpacing.fixed(10)
+        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 }
 
+extension DiaryViewController: WriteDiaryDelegateProtocol {
+    func didSelectRegister(diary: Diary) {
+        self.diaryList.append(diary)
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(diaryList, toSection: .main)
+        dataSource.apply(snapshot)
+    }
+}
