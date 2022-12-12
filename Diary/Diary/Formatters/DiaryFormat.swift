b1e8481645e82f20e3f1f32b094919937ed5f173
KayAhn0126 <dkseotn123@gmail.com>
2022-12-12 15:10:17 +0900

    refactor: Diary cell에 사용되는 날짜 형식을 클래스 싱글톤으로 구현

--- Diary/Diary/Formatters/DiaryFormat.swift
+++ Diary/Diary/Formatters/DiaryFormat.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+final class DiaryFormat {
+    static let shared = DiaryFormat()
+    
+    private func swapToDiaryFormat(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
+}
