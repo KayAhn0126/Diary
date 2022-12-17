50114bf557254e00a84a22f05dfe5e9475d8084f
KayAhn0126 <dkseotn123@gmail.com>
2022-12-17 22:27:07 +0900

    feat: star tab을 클릭하면 즐겨찾기된 일기들을 보여주는 로직 구현

--- Diary/Diary/Star/StarCollectionViewCell.swift
+++ Diary/Diary/Star/StarCollectionViewCell.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class StarCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    func configureCell(diary: Diary) {
+        self.titleLabel.text = diary.title
+        self.dateLabel.text = DiaryFormat.swapToDiaryFormat(date: diary.date, isSimplify: false)
+    }
 }
